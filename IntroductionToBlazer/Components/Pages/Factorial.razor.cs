d9ac07f3e443d8adf9bd15184d64a51196613a69
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2025-09-16 14:37:46 -0400

    TODOlist in progress

---
+++ IntroductionToBlazer/Components/Pages/Factorial.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace IntroductionToBlazer.Components.Pages
+{
+	public partial class Factorial
+	{
+		int n = 0;
+		long factorial = 1;
+
+		void Calculate()
+		{
+			factorial = 1;
+			for (int i = 1; i < n; i++)
+			{
+				factorial *= i;
+			}
+			//return factorial;
+		}
+	}
+}
