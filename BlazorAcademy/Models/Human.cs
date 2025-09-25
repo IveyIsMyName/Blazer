9a273a4d01bf30622fc149db199a91e3b445350f
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2025-09-25 14:53:47 -0400

    Teachers and Students in progress

---
+++ BlazorAcademy/Models/Human.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.CodeAnalysis;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace BlazorAcademy.Models
+{
+	public class Human
+	{
+		[Required]
+		[Display(Name="Фамилия")]
+		public string last_name { get; set; }
+		[Required]
+		public string first_name { get; set; }
+		public string? middle_name { get; set; }
+		[Required]
+		public DateOnly birth_date { get; set; }
+		[EmailAddress]
+		public string? email { get; set; }
+		[Phone]
+		public string? phone { get; set; }
+		public byte[]? photo { get; set; }
+	}
+}
--- BlazorAcademy/Models/Teacher.cs
+++ BlazorAcademy/Models/Teacher.cs
@@ -1,16 +1,17 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-
 namespace BlazorAcademy.Models
 {
-	public class Direction
+	public class Teacher:Human
 	{
 		[Key]
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public byte direction_id { get; set; }
+		public short teacher_id { get; set; }
 		[Required]
-		public string? direction_name { get; set; }
+		public DateOnly work_since { get; set; }
+		[Required]
+		public decimal rate { get; set; }
 	}
 }
